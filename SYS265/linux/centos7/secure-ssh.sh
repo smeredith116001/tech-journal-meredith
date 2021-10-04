5e23519dd30cfcdab2336778079ae585e74b4205
root <root@web01-sebastian.sebastian.local>
2021-10-04 19:24:34 -0400

    edits

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,5 @@
+#secure-ssh.sh
+#author smeredith116001
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
